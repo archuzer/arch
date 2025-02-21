8a1928e7d2b8e4b3da985ccba8b8aed3e6c9cf11
archuzer <archuzer1@gmail.com>
2025-02-21 16:10:11 +0000

    Update i.sh

--- i.sh
+++ i.sh
@@ -3,25 +3,21 @@
 # Print every line that executes
 set -x
 
-lsblk
-
-read -n 1 -s -r -p "Press any key to continue..."
 parted /dev/nvme0n1 mklabel gpt
 parted /dev/nvme0n1 mkpart primary ext4 0GB 1GB
 parted /dev/nvme0n1 mkpart primary ext4 1GB 100%
-read -n 1 -s -r -p "Press any key to continue..."
 lsblk
-read -n 1
+sleep 5
 mkfs.vfat /dev/nvme0n1p1
 mkfs.ext4 /dev/nvme0n1p2
-read -n 1 -s -r -p "Press any key to continue..."
-
+lsblk
+sleep 5
 mount /dev/nvme0n1p2 /mnt
 mount --mkdir /dev/nvme0n1p1 /mnt/boot
 lsblk
-read -n 1 -s -r -p "Press any key to continue..."
+sleep 5
 
-pacstrap -K /mnt base linux linux-firmware intel-ucode sudo vim grub efibootmgr networkmanager xorg xorg-xinit base-devel i3 alacritty chromium htop rofi
+pacstrap -K /mnt base linux linux-firmware intel-ucode sudo vim curl grub efibootmgr networkmanager xorg xorg-xinit base-devel libva-intel-driver intel-media-driver vulkan-intel i3 alacritty chromium htop tmux rofi git fzf pipewire pipewire-alsa pipewire-pulse pipewire-jack inxi zip unzip thunar file-roller pavucontrol vlc
 
 arch-chroot /mnt
 #useradd -mG wheel -s /bin/bash d
@@ -37,5 +33,6 @@ arch-chroot /mnt
 
 #sed -i '58 s/^/#/' ~/.config/i3/config
 #sed -i '60 s/^#//' ~/.config/i3/config
-xrandr --output HDMI1 --above eDP1 > /mnt/home/d/.xinitrc
-exec i3 >> /mnt/home/d/.xinitrc
+sed -i '125s/^#/ /' /mnt/etc/sudoers
+echo "xrandr --output HDMI1 --above eDP1" > /mnt/home/d/.xinitrc
+echo "exec i3" >> /mnt/home/d/.xinitrc
