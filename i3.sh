8106bd145d9753c12e2de35723905af0e4eacdaa
archuzer <archuzer1@gmail.com>
2025-02-22 19:09:46 +0000

    Create i3.sh

---
+++ i3.sh
@@ -0,0 +1,8 @@
+sudo pacman -S xorg xorg-xinit base-devel sed chromium firefox libva-intel-driver vulkan-intel i3 alacritty brightnessctl ranger htop tmux rofi git fzf pipewire pipewire-alsa pipewire-pulse pipewire-jack inxi zip unzip thunar file-roller pavucontrol vlc ffmpeg xf86-video-intel
+
+mkdir -p /home/d/.config/i3
+cp /etc/i3/config /home/d/.config/i3/config
+sleep 1c
+chown d:d /home/d/.config/i3/config
+sed -i '58 s/^/#/' /home/d/.config/i3/config
+sed -i '60 s/^#//' /home/d/.config/i3/config
