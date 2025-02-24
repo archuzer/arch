2194ae4041d83893b27e94368497e198232050f6
archuzer <archuzer1@gmail.com>
2025-02-24 09:30:00 +0000

    Delete i4.sh

--- i4.sh
+++
@@ -1,6 +0,0 @@
-parted /dev/nvme0n1 mklabel gpt
-parted /dev/nvme0n1 mkpart primary ext4 0GB 1GB
-parted /dev/nvme0n1 mkpart primary ext4 1GB 100%
-
-mkfs.vfat /dev/nvme0n1p1
-mkfs.ext4 /dev/nvme0n1p2
