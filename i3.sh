7caa2ed1230d382246890335323b068eb8112f05
archuzer <archuzer1@gmail.com>
2025-02-24 09:30:07 +0000

    Delete i3.sh

--- i3.sh
+++
@@ -1,8 +0,0 @@
-sudo pacman -S xorg xorg-xinit base-devel sed chromium firefox libva-intel-driver vulkan-intel i3 alacritty brightnessctl ranger htop tmux rofi git fzf pipewire pipewire-alsa pipewire-pulse pipewire-jack inxi zip unzip thunar file-roller pavucontrol vlc ffmpeg xf86-video-intel
-
-mkdir -p /home/d/.config/i3
-cp /etc/i3/config /home/d/.config/i3/config
-sleep 1
-chown d:d /home/d/.config/i3/config
-sed -i '63 s/^/#/' /home/d/.config/i3/config
-sed -i '65 s/^#//' /home/d/.config/i3/config
